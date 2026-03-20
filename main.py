22ec3a939689d96a211c817625d2f97ccae5d180
a.skochek <aleksandrskochek@gmail.com>
2026-03-20 20:38:16 +0300

    Урезан до минимальных настроек

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
+
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Логирование
 logging.basicConfig(
@@ -25,27 +27,39 @@ keyword_file_path = os.path.join(current_dir, "keywords.txt")
 with open(keyword_file_path) as file:
     keywords = [line.strip() for line in file]
 
+sender = ""
 # Бизнес-логика
 async def main() -> None:
     # создаём клиент и логинимся (при первом запуске спросит номер телефона в международном формате + пароль 2FA)
-    async with TelegramClient("session", API_ID, API_HASH) as client:
-        if client.is_connected():
-            print("Connected")
-            logging.info("Подключение к клиенту выполнено успешно")
+    async with (
+        TelegramClient("session", API_ID, API_HASH) as client,
+        TelegramClient("bot_session", API_ID, API_HASH) as bot,
+                ):
+        await bot.start(bot_token=BOT_TOKEN)
+        if client.is_connected() and bot.is_connected():
+            logging.info("Подключение к клиенту и боту выполнено успешно")
 
-        @client.on(events.NewMessage(chats="ezio_test"))
-        async def forward_to_me(event):
+        @bot.on(events.NewMessage(pattern=r"/start"))
+        async def start(event: events.NewMessage.Event):
+            sender_id = event.sender_id
+            await event.respond("Привет!🖐🏻\n\nЭтот бот будет пересылать тебе сообщения о новых вакансиях по маркетингу")
+
+        @client.on(events.NewMessage())
+        async def forward_to_bot(event):
             if any(w in event.raw_text for w in keywords):
                 message_id = event.message.id
                 chat_name = event.chat.username
-                message = event.message.message
+                message = event.message
                 logging.info(f"Получено сообщение {message_id} из {chat_name}")
                 await event.message.forward_to("me")
+                logging.info(f"Сообщение переслано в избранное")
+                await bot.send_message(entity=8105768964, message=message)
 
 
         print("🚀 Forwarder запущен.")
         logging.info("Forwarder запущен")
         await client.run_until_disconnected()
+        await bot.run_until_disconnected()
 
 # Запуск
 if __name__ == "__main__":
@@ -54,5 +68,4 @@ if __name__ == "__main__":
     except ForbiddenError:
         logging.info("Нарушение настроек приватности")
     except (KeyboardInterrupt, SystemExit):
-        print("👋 До встречи!")
         logging.info("Клиент отключен")
